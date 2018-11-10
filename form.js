e401f000037e6a3f91f4f19a88ee840b0c641f71
Nielene Thomas <nielenethomas@pursuit.org>
2018-11-10 17:24:01 -0500

    4th commit

--- form.js
+++ form.js
@@ -1,3 +1,43 @@
 document.addEventListener("DOMContentLoaded", () => {
-  
+  // debugger
+  // function showSelection(node) {
+  //   node.classList.remove("choice")
+  //   node.classList.add("chosen")
+  // }
+
+console.log(document.querySelector(".choice").innerText);
+
+  // let button = document.querySelectorAll(".choice");
+  //
+  // button.addEventListener("click", (event) => {
+  //   debugger
+  //   showSelection(button)
+
+
+
+    // button.style.backgroundColor = "yellow"
+
+  // let results = document.querySelectorAll(".results");
+  //
+  // results.addEventListener("click", (event) => {
+  //   results.style.backgroundColor = "yellow"
+
+
+
+
+  // })
+
+
+  // let input =
+  //
+  // if () {
+  //
+  // }
+
+
+
+
+
+
+
 })
