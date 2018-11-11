c48ac8050adadafe2752dcb0ebcd66fca527239f
Treagan Birbal <treaganbirbal@pursuit.org>
2018-11-11 13:53:29 -0500

    Treagan Birbal

---
+++ index.js
@@ -0,0 +1,39 @@
+
+// document.addEventListener("DOMContentLoaded", () => {
+// window.alert("The Page Is Fully Loaded like a Potato")
+// })
+
+  window.alert("Let's Find Out The Type of Crazy The Lives Inside of You by Taking A Random Ass Quiz That Does Not Really Test For Anything and The Two Are Totally Unrelated!!!");
+
+  let question = window.prompt("What first comes to my when you think cheese?")
+let quiz = document.querySelector(".idk");
+let question = question[i];
+let answer = qestion[i].answer;
+idk.addEventListener("click" , event => {
+  let question = question[i];
+  let answer = qestion[i].answer;
+
+  // window.prompt("What first comes to my when you think cheese?")
+})
+
+// let button = document.querySelector(".wind-up")
+//
+// button.addEventListener("click", event => {
+//
+// let countdown = document.querySelector(".countdown");
+// let show = document.querySelector(".jack-hiding");
+// let jackOut = document.querySelector(".jack-message")
+//  let num = 4
+//   let interval = setInterval(function() {
+//     countdown.innerText = num;
+//     if (num === 0) {
+//       clearInterval(interval);
+//       show.classList.remove("jack-hiding");
+//       show.classList.add("jack-showing");
+//       jackOut.classList.remove("jack-message")
+//       jackOut.classList.add("creepy-text");
+//       jackOut.innerText = "HERE'S JACK!";
+//     }
+//     num--;
+//   }, 1000);
+// })
