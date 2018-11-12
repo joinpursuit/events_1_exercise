af301cf972fefc90d7b394f6fb4668442a451743
Morteza Khaki <mortezakhaki@Mortezas-MacBook-Air.local>
2018-11-11 22:25:07 -0500

    answer

---
+++ index2.js
@@ -0,0 +1,23 @@
+document.addEventListener("DOMContentLoaded", () => {
+  let button = document.querySelector(".freeTheJack");
+  button.addEventListener("click", () => {
+    let count = document.querySelector(".countdown");
+    let jack = document.querySelector(".jack-hiding");
+    console.log(count);
+
+    let num = Number(count.innerText);
+    console.log(num);
+
+    let timer = setInterval(() => {
+      count.innerText = `${num}`;
+      num--;
+      console.log(num);
+      if (num === 0) {
+        clearInterval(timer);
+        jack.classList.remove("jack-hiding");
+        console.log(jack);
+        jack.classList.add("jack-revealed");
+      }
+    }, 1000);
+  });
+});
