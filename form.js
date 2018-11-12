eed32f96c2c79d1e6ab6412c94768209875bde41
Nielene Thomas <nielenethomas@pursuit.org>
2018-11-12 10:13:09 -0500

    6th commit

--- form.js
+++ form.js
@@ -10,24 +10,40 @@ document.addEventListener("DOMContentLoaded", () => {
   // let q1 = document.querySelectorAll("h1");
   //
   // q1.addEventListener("click", (event) => {
-  //      q1.style.backgroundColor = "yellow"
+  //   // q1.
+  //   q1.style.backgroundColor = "yellow"
   //   // debugger
   //   // showSelection(button)
   // });
 
-let choice = document.querySelectorAll(".choice");
-console.log(choice).innerHTML;
-let result = document.querySelector(".results").innerHTML;
-console.log(result);
+
+
+// let choice = document.querySelectorAll(".choice");
+// console.log(choice).innerText;
+// let resultDiv = document.querySelector(".results").innerHTML;
+// console.log(resultDiv);
+
+
 
   // button.style.backgroundColor = "yellow"
 
   // let results = document.querySelectorAll(".results");
   //
-  // results.addEventListener("click", (event) => {
-  //   results.style.backgroundColor = "yellow"
+  // resultDiv.addEventListener("click", (event) => {
+  //   resultDiv.style.backgroundColor = "yellow"
 
 
+
+// let clickMe = document.querySelector(".clickMe")
+// let
+// clickMe.addEventListener("click", (event) => {
+//
+//   function showResultDiv (node) {
+//     node.display = block;
+//   }
+//
+//   showResultDiv()
+// })
 
 
   // })
@@ -39,24 +55,33 @@ console.log(result);
   //
   // }
 
-  // let objResults = {
-  //   1a: 0,
-  //   1b: 0,
-  //   1c: 0,
-  //   1d: 0,
-  //   2a: 0,
-  //   2b: 0,
-  //   2c: 0,
-  //   2d: 0,
-  //   3a: 0,
-  //   3b: 0,
-  //   3c: 0,
-  //   3d: 0,
-  //   4a: 0,
-  //   4b: 0,
-  //   4c: 0,
-  //   4d: 0,
-  // };
+
+
+  let objPersonalStyle =
+  [
+    {a1a: "You live for the now"},
+    {a1b: "You have simple tastes"},
+    {a1c: "You enjoy comfort and warmth"},
+    {a1d: "You are self-sacrificing to a fault"},
+
+    {a2a: ", yet are very conscientious."},
+    {a2b: ", yet do hold some thought for the future."},
+    {a2c: ", yet might benefit with some thought of the furture."},
+    {a2d: ", yet you are a risk-taker."},
+
+    {a3a: "Also, you care about the environment"},
+    {a3b: "Also, you are a puritan"},
+    {a3c: "Also, you are passionate"},
+    {a3d: "Also, you stay in your own head too much"},
+
+    {a4a: "and you care more about happiness than cost."},
+    {a4b: "and you don't like living in regret."},
+    {a4c: "and you are obsessed with your privacy."},
+    {a4d: "and you are very practical."}
+  ]
+
+
+  console.log(objPersonalStyle.a2a);
 
 
 
