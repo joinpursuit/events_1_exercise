352829f1f2d39ee1e8041010ce72bb915f4836e5
Deyvi <deyvio@gmail.com>
2018-11-16 10:19:14 -0500

    more refactoring

--- question3-index.js
+++ question3-index.js
@@ -1,43 +1,71 @@
-document.addEventListener('DOMContentLoaded', () => {
+const options = [
+  {title: "Always", value: 1},
+  {title: "Sometimes", value: 0},
+  {title: "Never", value: -1},
+]
 
+const quizData = [
+  { question: "Does spending time in nature relax and recharge you?",
+    options: options,
+  },
+  { question: "Do you get bored quickly?" ,
+    options: options,
+  },
+  { question: "Do you find yourself 'feeling' other people's emotions?" ,
+    options: options,
+  },
+  { question: "DO your friends often unload their problems onto you?" ,
+    options: options,
+  },
+  { question: "Do you become overwhelmed in close relationships?" ,
+    options: options,
+  },
+]
+
+document.addEventListener('DOMContentLoaded', () => {
   let form = document.querySelector('form');
   let body = document.querySelector('body');
   let result = document.createElement('h2');
 
-  let sum = 0
+  generateQuizHtml(quizData)
 
-  // form.addEventListener('change', (event) => {
-  //   debugger
-  //
-  // })
+  form.addEventListener('change', (event) => {
+    console.log(event)
+  })
 
   form.addEventListener('submit', (event) => {
     event.preventDefault();
 
-    let checkedValue1 = document.querySelector('input[name="answer1"]:checked').value;
-    let checkedValue2 = document.querySelector('input[name="answer2"]:checked').value;
-    let checkedValue3 = document.querySelector('input[name="answer3"]:checked').value;
-    let checkedValue4 = document.querySelector('input[name="answer4"]:checked').value;
-    let checkedValue5 = document.querySelector('input[name="answer5"]:checked').value;
-console.log('hiii');
-    sum = sum + (+checkedValue1);
-    sum = sum + (+checkedValue2);
-    sum = sum + (+checkedValue3);
-    sum = sum + (+checkedValue4);
-    sum = sum + (+checkedValue5);
+    const checkedValues = quizData.map((x, i) => {
+      return document.querySelector(`input[name="answer${i}"]:checked`).value
+    })
+    const sum = checkedValues.reduce((acc, cur) => acc + cur)
 
-    if (sum > 2) {
-      result.innerText = 'You are a carebear :)';
-      body.append(result);
-    } else if (sum <= 2 && sum > -2) {
-      result.innerText = 'In the middle of the pack';
-      body.append(result);
-    } else {
-      result.innerText = 'Work on caring brah.';
-      body.append(result);
-    }
-
-
+    gradeQuiz(sum, result, body)
   });
-
 });
+
+function generateQuizHtml(quizData) {
+  const quizContainer = document.querySelector('.quiz')
+
+  quizContainer.innerHTML = quizData.map((x, i) => `
+      <p>${x.question}</p>
+      <input class="messageCheckbox1" type="radio" name="answer${i}" value="${x.options[0].value}"> ${x.options[0].title}
+      <input class="messageCheckbox1" type="radio" name="answer${i}" value="${x.options[1].value}"> ${x.options[1].title}
+      <input class="messageCheckbox1" type="radio" name="answer${i}" value="${x.options[2].value}"> ${x.options[2].title}
+    `
+  )
+}
+
+function gradeQuiz(sum, result, body) {
+  if (sum > 2) {
+    result.innerText = 'You are a carebear :)';
+    body.append(result);
+  } else if (sum <= 2 && sum > -2) {
+    result.innerText = 'In the middle of the pack';
+    body.append(result);
+  } else {
+    result.innerText = 'Work on caring brah.';
+    body.append(result);
+  }
+}
